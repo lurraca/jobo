a2b5680dcd2874f2823368a16d94784cb8742442
Luis Urraca <l.urraca@gmail.com>
2014-02-10 02:07:47 -0400

    Adding validation to jobs that a category must be presence.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ActiveRecord::Base
 
   belongs_to :category
+  validates :category, presence: true
 
   def self.create_job(location, position, company, category)
     create!(location:location,
