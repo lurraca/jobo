7f2202d5313deaf9d8496963ab5215816080edf6
Luis Urraca <l.urraca@gmail.com>
2014-02-10 02:06:18 -0400

    Adding a method to fetch all parent categories + Test for category model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,8 @@ class Category < ActiveRecord::Base
   has_many :subcategories, class_name: "Category", foreign_key: "parent_id"
   belongs_to :parent_category, class_name: "Category", foreign_key: "parent_id"
 
+  def self.elders
+    where(parent_id: nil)
+  end
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:parent_1) { FactoryGirl.create(:category) }
+  let(:parent_2) { FactoryGirl.create(:category) }
+  let(:subcategory_1) { FactoryGirl.create(:subcategory, parent_category: parent_1) }
+  let(:subcategory_2) { FactoryGirl.create(:subcategory, parent_category: parent_1) }
+  let(:subcategory_3) { FactoryGirl.create(:category, parent_category: parent_2) }
+
+
+
+  it "returns only the parents categories" do
+    expect(Category.elders).to include(parent_1, parent_2)
+    expect(Category.elders).not_to include(subcategory_1, subcategory_2, subcategory_3)
+  end
+
+  it "returns appropiate children for parents categories" do
+    expect(parent_1.subcategories).to include(subcategory_1, subcategory_2)
+    expect(parent_1.subcategories).not_to include(subcategory_3)
+  end
+
+
 end
